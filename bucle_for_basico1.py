614c777fc1bdd89cd27772af91e1d525fcd2402b
Vicente Chirinos <vchirinosp10@gmail.com>
2023-05-12 12:20:57 -0500

    funciones basicas 1 y mejorar el primer ejercicio Core

--- bucle_for_basico1.py
+++ bucle_for_basico1.py
@@ -1,9 +1,10 @@
+#Primer Ejercicio
 for i in range(151):
     print(i)
-
+#Segundo Ejercicio
 for i in range(5, 1001, 5):
     print(i)
-
+#Tercero Ejercicio
 for i in range(1, 101):
     if i % 10 == 0:
         print("Coding Dojo")
@@ -11,15 +12,15 @@ for i in range(1, 101):
         print("Coding")
     else:
         print(i)
-
+#Cuarto Ejercicio
 suma = 0
 for i in range(1, 500001, 2):
     suma += i
 print(suma)
-
+#Quinto Ejercicio
 for i in range(2018, 0, -4):
     print(i)
-
+#Sexto Ejercicio
 lowNum = 2
 highNum = 9
 mult = 3
